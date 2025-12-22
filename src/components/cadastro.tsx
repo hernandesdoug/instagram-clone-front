32735a6b44191c331e62403bf365a54af99f655d
Douglas Hernandes <douglashernandes@icloud.com>
2025-12-22 15:53:45 -0300

    ajustando tela de cadastro

--- src/components/cadastro.tsx
+++ src/components/cadastro.tsx
@@ -1,29 +1,103 @@
 import type { cadastroProps } from "./cadastro.ts";
 import React, { useState } from 'react';
 import Footer from "./footer.tsx";
+import api from "../services/api.ts";
+import styled from "styled-components";
 
 const Cadastro: React.FC = () => {
-    const [usuario, setUsuario] = useState<cadastroProps>();
-    const [senha, setSenha] = useState<cadastroProps>();
-    const [nomeCompleto, setNomeCompleto] = useState<cadastroProps>();
-    const [nomeUsuario, setNomeUsuario] = useState<cadastroProps>();
-    
-     const handleCadastro = () => {
-        
+    const [dadosCadastro, setDadosCadastro] = useState<cadastroProps>({
+        usuario: "",
+        senha: "",
+        nomeCompleto: "",
+        nomeUsuario: ""
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setDadosCadastro(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+    const handleCadastro = async () => {
+        try {
+            const response = await api.post("/login", {});
+            if (response.status === 200) {
+
+            } else {
+                console.log("Login Failed!", response.status);
+            }
+        } catch (error) {
+            console.error("Unexpected error!", error);
+        }
     }
     return (
         <>
-        <div>
             <div>
-
+                <CadastroForm>
+                    <h1>Instagram</h1>
+                    <p>Cadastre-se para ver fotos e vídeos dos seus amigos.</p>
+                    <Inputs type="text"
+                        id="usuario"
+                        placeholder="Número do celular ou email"
+                        value={dadosCadastro.usuario}
+                        onChange={handleChange}
+                        required
+                    />
+                    <Inputs type="password"
+                        id="password"
+                        placeholder="Senha"
+                        value={dadosCadastro.senha}
+                        onChange={handleChange}
+                        required
+                    />
+                    <Inputs type="text"
+                        id="usuario"
+                        placeholder="Nome completo"
+                        value={dadosCadastro.nomeCompleto}
+                        onChange={handleChange}
+                        required
+                    />
+                    <Inputs type="text"
+                        id="usuario"
+                        placeholder="Nome de usuário"
+                        value={dadosCadastro.nomeUsuario}
+                        onChange={handleChange}
+                        required
+                    />
+                    <BtnCadastrar onClick={handleCadastro}>Cadastre-se</BtnCadastrar>
+                    <div>
+                        <p>Tem uma conta?</p>
+                        <a href="">Conecte-se</a>
+                    </div>
+                </CadastroForm>
+                <Footer></Footer>
             </div>
-            <div>
-                
-            </div>
-            <Footer></Footer>
-        </div>
         </>
     )
 }
 
 export default Cadastro;
+
+const CadastroForm = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    justify-content: center;
+    margin: 10px;
+`;
+
+const Inputs = styled.input`
+  width: 250px;
+  height: 30px;
+  border-radius: 0.2rem;
+`;
+
+const BtnCadastrar = styled.button`
+  width: 250px;
+  height: 30px;
+  border-radius: 0.4rem;
+  background-color: #00a2ff;
+  color: #ffffff;
+`;
